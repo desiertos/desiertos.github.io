244d62386ffc8b3ecc84b109bc0ccbf0260ce1aa
tiagombp <tiagombp@gmail.com>
2021-05-12 00:03:04 -0300

    now with cities

--- capa/capa.js
+++ capa/capa.js
@@ -2,79 +2,113 @@ const capa = {
 
     map : {
 
-        file : 'provv.geojson',
+        files : [
+            
+            'provv.geojson',
+            'mun_.geojson'
 
-        render : function() {
+        ],
 
-            let data = capa.data;
+        data : {
 
-            let feats = data.features;
-            //   topojson.feature(
-            //     topodata, 
-            //     topodata.objects.provincia)
-            //   .features;
+            prov : null,
+            mun : null
+    
+        },
 
-            let geodata = {
-
-                type:"FeatureCollection",
-                "features": feats
-
-            };
+        proj : () => {
 
             let h = capa.utils.dims.height;
             let w = capa.utils.dims.width;
-
-            let proj = d3.geoTransverseMercator()
+            
+            return d3.geoTransverseMercator()
               .center([0, -38.5])
               .rotate([60, 0])
               .scale(750)
-              .translate([w / 2, h / 2]);
+              .translate([w / 2, h / 2])
+        },
 
-            //let proj = d3.geoTransverseMercator()
-            //   .center([2.5, -38.5])
-            //   .rotate([66, 0])
-            //   .scale(200)
-            //   //.scale((h * 56.5) / 33)
-            //   .translate([(w / 2), (h / 2)]);
+        render : {
+            
+            prov : function() {
 
-            // let proj = d3
-            //   .geoMercator()
-            //   //.geoConicEqualArea()
-            //   //.parallels([-56, -21])
-            //   .rotate([40, 0])
-            //   .fitSize([h, w], geodata)
+                let data = capa.map.data.prov;
 
+                console.log(data);
 
-            let svg = d3.select("svg#vis-capa");
+                let feats = data.features;
+                //   topojson.feature(
+                //     topodata, 
+                //     topodata.objects.provincia)
+                //   .features;
 
+                let proj = capa.map.proj();
 
-            svg.append("g")
-              .selectAll("path")
-              .data(feats)
-              .join("path")
-              .attr("fill", 'hotpink')
-              .attr("stroke", 'khaki')
-              .attr("d", d3.geoPath().projection(proj))
-            ;
+                //console.log(proj)
 
+                let svg = d3.select("svg#vis-capa");
 
+                svg.append("g")
+                .selectAll("path")
+                .data(feats)
+                .join("path")
+                .attr("fill", 'hotpink')
+                .attr("stroke", 'khaki')
+                .attr("d", d3.geoPath().projection(proj))
+                ;
+
+            },
+
+            mun : function() {
+
+                let data = capa.map.data.mun;
+
+                let feats = data.features;
+                //   topojson.feature(
+                //     topodata, 
+                //     topodata.objects.provincia)
+                //   .features;
+
+                let proj = capa.map.proj();
+
+                let svg = d3.select("svg#vis-capa");
+
+                //console.log(feats[0], proj(feats[0].geometry.coordinates));
+
+                svg.selectAll("circle")
+                    .data(feats)
+                    .join("circle")
+                    .attr("fill", 'transparent')
+                    .attr("stroke", 'dodgerblue')
+                    .attr("cx", d => d.geometry ? proj(d.geometry.coordinates)[0] : 0)
+                    .attr("cy", d => d.geometry ? proj(d.geometry.coordinates)[1] : 0)
+                    .attr("r", 2)
+                ;
+
+            }
+            
         }
 
     },
 
-    data : {
+    vis : {
 
-        map : null
+        scales : {
 
+            colors : (cat) => Object.values(capa.utils.colors.categories)[+cat-1]
+
+        }
     },
 
     utils : {
 
         read_data : function() {
 
-            d3.json(capa.map.file).then(file => {
+            Promise.all(capa.map.files.map(filename => d3.json(filename)))
+              .then(files => {
 
-                capa.data = file;
+                capa.map.data.prov = files[0];
+                capa.map.data.mun  = files[1];
 
                 capa.ctrl.begin();
 
@@ -103,6 +137,37 @@ const capa = {
             }
 
 
+        },
+
+        colors : {
+
+            categories : {
+
+                desierto : null,
+                semidesierto : null,
+                semibosque : null,
+                bosque : null
+    
+            },
+
+            get_from_css : function(color) {
+
+                const style = getComputedStyle( document.documentElement );
+                const value = style.getPropertyValue( '--color-' + color );
+                return value;
+    
+            },
+
+            populate : function() {
+
+                Object.keys(capa.utils.colors.categories).forEach(color => {
+
+                    capa.utils.colors.categories[color] = capa.utils.colors.get_from_css(color);
+    
+                });
+
+            }
+    
         }
  
     },
@@ -110,6 +175,8 @@ const capa = {
     ctrl : {
 
         init : function() {
+
+            capa.utils.colors.populate();
 
             capa.utils.read_data();
 
@@ -119,7 +186,8 @@ const capa = {
 
 
             capa.utils.dims.get();
-            capa.map.render();
+            capa.map.render.prov();
+            capa.map.render.mun();
 
         }
 
